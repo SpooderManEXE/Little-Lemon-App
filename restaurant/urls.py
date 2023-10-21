f971db92b57679367e3642e9c22d0db2296a1d3e
unknown <n.sultan001@gmail.com>
2023-10-21 14:54:54 +0530

    initial commit

---
+++ restaurant/urls.py
@@ -0,0 +1,19 @@
+from . import views
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>/', views.display_menu_item, name="menu_item"),  
+    path('book/', views.book, name="book"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('bookings', views.bookings, name='bookings'), 
+    path('api/menu-items/', views.MenuItemView.as_view()),
+    path('api/menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('api/bookings/', views.BookingView.as_view()),
+    path('api/bookings/<int:pk>', views.SingleBookingView.as_view()),
+   
+]
